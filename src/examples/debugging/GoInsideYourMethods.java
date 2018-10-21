fcb026cdf76df533225631271dd856e05b716395
pawel-szczesny <pawelszczesny0@gmail.com>
2018-10-21 15:54:18 +0200

    some refactor of debuggin and exceptions examples

--- src/examples/debugging/GoInsideYourMethods.java
+++ src/examples/debugging/GoInsideYourMethods.java
@@ -15,7 +15,7 @@ public class GoInsideYourMethods {
 	    Random random = new Random();
 	    for(int i = 0; i < numberOfWords; i++)
 	    {
-	        char[] word = new char[random.nextInt(8)+3]; // words of length 3 through 10. (1 and 2 letter words are boring.)
+	        char[] word = new char[random.nextInt(8)+3]; // words of length 3 through 10.
 	        for(int j = 0; j < word.length; j++)
 	        {
 	            word[j] = (char)('a' + random.nextInt(26));
--- src/examples/exceptions/BestPracticesOfExceptions.java
+++ src/examples/exceptions/BestPracticesOfExceptions.java
@@ -1,16 +1,25 @@
 package examples.exceptions;
 
+import java.util.Random;
+
 import examples.exceptions.our_exceptions.OurException;
 
 public class BestPracticesOfExceptions {
 	public static void main(String[] args) {
 		lookForTheMostSpecificException();
+		
+		// if we know what was the reason of exception is good to pack it with our own exception
+		
+		// Example 1: Caught exception and thrown own
 		ifPossibleUseOwnExceptions();
 		ifPossibleUseOwnExceptionsWithOwnMessage();
+		
+		// Example 2: If some logic is not expect - thrown own Exception
+		ifPossibleUseOwnExceptions_2();
+		
 	}
 	
 	public static void ifPossibleUseOwnExceptions() {
-		// if we know what was the reason of exception is good to pack it with our own exception
 		try {
 			throw new RuntimeException("Something went wrong!");
 		} catch (RuntimeException e) {
@@ -20,16 +29,25 @@ public class BestPracticesOfExceptions {
 	}
 	
 	public static void ifPossibleUseOwnExceptionsWithOwnMessage() {
-		// if we know what was the reason of exception is good to pack it with our own exception
 		try {
 			throw new RuntimeException("Something went wrong!");
 		} catch (RuntimeException e) {
 			// Do something
-			throw new OurException("With our meaningfull message. " + "Original Exception: " + e.getMessage());
+			throw new OurException("With our meaningfull message. " + " Original Exception: " + e.getMessage());
 		}
 	}
 	
+	public static void ifPossibleUseOwnExceptions_2() {
+		if(someLogic()) {
+			throw new OurException("With our meaningfull message!");
+		}
+	}
+	
+	private static boolean someLogic() {
+		return new Random().nextBoolean();
+	}
+	
 	public static void lookForTheMostSpecificException() {
-		//If is it possible catch only 'expected' exceptions, not ALL Exception
+		// If is it possible catch only 'expected' exceptions, not ALL Exception
 	}
 }
