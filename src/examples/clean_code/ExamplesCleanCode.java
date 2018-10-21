6764e26a291f7c0fbe9f91daee3cda7eb09a2d81
pawel-szczesny <pawelszczesny0@gmail.com>
2018-10-21 21:17:18 +0200

    added examples for clean_code

---
+++ src/examples/clean_code/ExamplesCleanCode.java
@@ -0,0 +1,37 @@
+package examples.clean_code;
+
+public class ExamplesCleanCode {
+
+	public static void main(String[] args) {
+		
+
+		// WRONG Approches
+		
+		// 1. Commented lines of code
+		// example:
+		
+		// driver.click()
+		
+	}
+	
+	public void someMethod(String firstArgument, String second, String nextArgument, int someInt) {
+		// To many arguments - try to minimalize it
+	}
+	
+	public void methodDoingThatAndSomeOtherThing() {
+		// When you need to use 'and' word in name of method, please re-think it, maybe it will be better to split it into two methods.
+	}
+	
+	public boolean returnBooleanValueWrong() {
+		if ("a".equals("a")) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public boolean returnBooleanValueGood() {
+		return "a".equals("a");
+	}
+	
+}
