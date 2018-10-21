0262aec69d3b0bff322db1e6ee0d04af4c7470c1
pawel-szczesny <pawelszczesny0@gmail.com>
2018-10-21 22:46:12 +0200

    added exercise3_working_with_strings

---
+++ src/exercise3_working_with_strings/Exercise3_1.java
@@ -0,0 +1,6 @@
+package exercise3_working_with_strings;
+
+public class Exercise3_1 {
+	public static void main(String[] args) {
+	}
+}
---
+++ src/exercise3_working_with_strings/Exercise3_2.java
@@ -0,0 +1,4 @@
+package exercise3_working_with_strings;
+
+public class Exercise3_2 {
+}	
