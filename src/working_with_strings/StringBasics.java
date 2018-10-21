245b16125f3337ffc46018632a690a3fa1020441
pawel-szczesny <pawelszczesny0@gmail.com>
2018-10-21 16:30:13 +0200

    added examples for working_with_strings

---
+++ src/working_with_strings/StringBasics.java
@@ -0,0 +1,37 @@
+package working_with_strings;
+
+public class StringBasics {
+	private final static String SOME_EXAMPLE_STRING = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
+													+ "Cras vel leo elit. Etiam rhoncus blandit vehicula. Morbi eget dui ante. "
+													+ "Aenean auctor est sit amet leo pulvinar gravida";
+			
+	
+	public static void main(String[] args) {
+		// Adding char to String
+		String someWord = "Hello World";
+		System.out.println(addSomeCharToString('!', someWord));
+		
+		// Syntax of +=
+		String someNumberAsString = "1";
+		someNumberAsString += "2";
+		someNumberAsString += 3;
+		// someNumberAsString += 4;
+		System.out.println(someNumberAsString);
+		
+		// Syntax od concat
+		String someWordToConcat = "Java";
+		someWordToConcat = someWordToConcat.concat("8 The Best!");
+		someWordToConcat.concat("8 The Worst!");
+		System.out.println(someWordToConcat);
+		
+		// Some methods available to String
+		System.out.println(SOME_EXAMPLE_STRING.length());
+		System.out.println(SOME_EXAMPLE_STRING.startsWith("Lorem"));
+		System.out.println(SOME_EXAMPLE_STRING.endsWith("Lorem ipsum"));
+		
+	}
+	
+	public static String addSomeCharToString(char someChar, String someString) {
+		return someString + someChar;
+	}
+}
---
+++ src/working_with_strings/StringBuilderVsStringBuffer.java
@@ -0,0 +1,36 @@
+package working_with_strings;
+
+public class StringBuilderVsStringBuffer {
+	public static void main(String[] args) {
+		// Same example from StringBasics (concat) using StirngBuilder
+		StringBuilder someWordToConcat = new StringBuilder("Java");
+		someWordToConcat = someWordToConcat.append("8 The Best!");
+		someWordToConcat.append("8 The Worst!");
+		System.out.println(someWordToConcat);
+		
+		// Simple Benchmark Test - StringBuffer vs StringBuilder
+		compareStringBufferToStirngBuilder();
+	}
+
+	private static void compareStringBufferToStirngBuilder() {
+		int n = 77777777;
+        long t;
+        
+        StringBuffer sbuff = new StringBuffer();
+        t = System.currentTimeMillis();
+        for (int i = n; i --> 0 ;) {
+        	sbuff.append("");
+        }
+        System.out.println("StringBuffer:");
+        System.out.println(System.currentTimeMillis() - t);
+        
+        StringBuilder sb = new StringBuilder();
+        t = System.currentTimeMillis();
+        for (int i = n; i > 0 ; i--) {
+            sb.append("");
+        }
+        System.out.println("StringBuilder:");
+        System.out.println(System.currentTimeMillis() - t);
+    
+	}
+}
